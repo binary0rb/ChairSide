ecbaee626e7ac3c564ce2ab92bb4d0b2c4fb83de
James Presley <jwpresley@gmail.com>
2026-07-23 11:55:12 -0500

    Add JavaScript linting guardrail

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+﻿import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["src/ChairSide.Board/wwwroot/*.js"],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        signalR: "readonly"
+      }
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: "error"
+    }
+  }
+]);
